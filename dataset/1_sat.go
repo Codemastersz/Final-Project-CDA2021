8d02022d5467fa28fd78958fbde4d50536d5c419
Codemastersz <xzhanglarch@gmail.com>
2021-10-26 23:57:11 -0400

    Update 1_sat.go

--- dataset/1_sat.go
+++ dataset/1_sat.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/Codemastersz/Final-Project-CDA2021/tree/main/dataset/lib"
+	"./lib"
 	"github.com/mitroadmaps/gomapinfer/common"
 	"github.com/mitroadmaps/gomapinfer/googlemaps"
 
